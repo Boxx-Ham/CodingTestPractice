01847eb77828bc28a93077760bec648fd5ae70b9
Boxx-Ham <sangja234@gamil.com>
2025-04-08 22:43:27 +0900

    2025-04-08 BoxxHam : 프로그래머스 문제 08~문제 10 풀이 완료

--- programmers/WormUpProblem/Stack/StackWormUpQuestion1.java
+++ programmers/WormUpProblem/Stack/StackWormUpQuestion1.java
@@ -7,8 +7,42 @@ package PROGRAMMERS.WormUpProblem.Stack;
  * 코딩 테스트 합격자 되기
  */
 
+import java.util.Stack;
+
 public class StackWormUpQuestion1 {
     public static void main(String[] args) {
-        
+        int inputDecimal1 = 10;
+        int inputDecimal2 = 27;
+        int inputDecimal3 = 12345;
+
+        String outputValue1 = solution(inputDecimal1);
+        String outputValue2 = solution(inputDecimal2);
+        String outputValue3 = solution(inputDecimal3);
+
+        System.out.println(inputDecimal1 + " -> " + outputValue1);
+        System.out.println(inputDecimal2 + " -> " + outputValue2);
+        System.out.println(inputDecimal3 + " -> " + outputValue3);
+    }
+
+    static String solution(int decimal) {
+        // 2진수를 담을 Stack 생성 및 리턴 값 담을 StringBuilder 생성
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        // decimal이 1보다 작아질 때까지 2진수 변환 작업 지속
+        while (decimal > 0) {
+            // decimal을 2로 나눈 나머지 stack에 저장
+            stack.push(decimal % 2);
+
+            // 다음 연산을 위해 decimal 2로 나눠줌
+            decimal /= 2;
+        }
+
+        // stack이 빌 때까지 하나씩 꺼내서 StringBuilder에 넣기
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
     }
 }
--- programmers/datastructure/Stack/PROGRAMMERS76502.java
+++ programmers/datastructure/Stack/PROGRAMMERS76502.java
@@ -7,8 +7,76 @@ package PROGRAMMERS.DataStructure.Stack;
  * 프로그래머스 76502번 (https://school.programmers.co.kr/learn/courses/30/lessons/76502)
  */
 
+import java.util.Stack;
+
 public class PROGRAMMERS76502 {
     public static void main(String[] args) {
-        
+        // 반례 추가 : "({[)]}"
+        String[] inputS = {"[](){}", "}]()[{", "[)(]", "}}}", "({[)]}"};
+
+        for (int i = 0; i < inputS.length; i++) {
+            System.out.println(inputS[i] + " -> " + solution(inputS[i]));
+        }
+    }
+
+    static int solution(String s) {
+        // s의 길이가 홀수이면 올바른 괄호 문자열이 절대 될 수 없으므로 미리 0 반환
+        if (s.length() % 2 == 1) return 0;
+
+        // 올바른 괄호 문자열의 개수를 담기 위한 변수 선언
+        int count = 0;
+
+        // x번 만큼 반복함
+        for (int x = 0; x < s.length(); x++) {
+            // x번 회전한 s 생성
+            String str = s.substring(x) + s.substring(0, x);
+
+            // str이 올바른 괄호 문자열인지 확인 : isValid() 함수 사용
+            // 만약 올바른 괄호 문자열이면 count 증가
+            if (isValid(str)) count++;
+        }
+
+        return count;
+    }
+
+    static boolean isValid(String s) {
+        // s의 첫 문자가 닫는 괄호이거나 s의 마지막 문자가 여는 괄호인 경우 올바른 괄호 문자열이 아니므로 false 리턴
+        int len = s.length();
+        if (s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']' || s.charAt(len - 1) == '(' || s.charAt(len - 1) == '{' | s.charAt(len - 1) == '[') {
+            return false;
+        }
+
+        // 올바른 괄호 문자열 판별을 위한 Stack 생성
+        Stack<Character> stack = new Stack<>();
+
+        // s가 올바른 괄호 문자열인지 판별
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+
+            // ch가 여는 괄호이면 stack에 넣기
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+
+            // 닫는 괄호인 경우
+            } else {
+                // stack이 비어있는데 ch가 닫는 괄호인 경우 올바른 괄호 문자열이 아니므로 false 리턴
+                if (stack.isEmpty()) {
+                    return false;
+
+                // stack이 비어있지 않다면 stack 안의 문자와 ch가 서로 같은 종류의 괄호인지 확인
+                } else {
+                    char open = stack.pop();
+
+                    // 서로 같은 종류의 괄호가 아니면 올바른 괄호 문자열이 아니므로 false 리턴
+                    if (!((open == '(' && ch == ')') || (open == '{' && ch == '}') || (open == '[' && ch == ']'))) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // stack이 비어있지 않으면 false, 비어있으면 true 반환
+        // 비어있으면 올바른 괄호 문자열이기 때문
+        return stack.isEmpty();
     }
 }
