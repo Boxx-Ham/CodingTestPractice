f78fe8694cff89d156ea3eedf05a88c977bdff00
Boxx-Ham <sangja234@gmail.com>
2025-04-14 20:30:31 +0900

    2025-04-14 BoxxHam : 백준 큐 문제 풀이

--- BAEKJOON/DataStructure/Queue/BAEKJOON11286.java
+++ BAEKJOON/DataStructure/Queue/BAEKJOON11286.java
@@ -7,6 +7,52 @@ package BAEKJOON.DataStructure.Queue;
  * 백준 11286번 (https://www.acmicpc.net/problem/11286)
  */
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.PriorityQueue;
+
+
 public class BAEKJOON11286 {
-    
+    public static void main(String[] args) throws IOException {
+        // 1. 연산의 개수 N 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 2. 우선순위 큐 선언 (정렬을 해야 하기 때문)
+        PriorityQueue<Integer> queue = new PriorityQueue<>((e1, e2) -> {
+            int firstAbs = Math.abs(e1);
+            int secondAbs = Math.abs(e2);
+
+            // 절대값이 같은 경우 음수가 먼저 큐에 들어가야 함
+            if (firstAbs == secondAbs) {
+                return e1 > e2 ? 1 : -1;
+            } else {
+                // 절대값이 작은 것이 먼저 큐에 들어가야 함
+                return firstAbs - secondAbs;
+            }
+        });
+
+        // 3. 로직 시작
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            // 0은 출력
+            if (input == 0) {
+                // 큐가 비어있다면 0 출력
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+
+                // 비어있지 않다면 제일 작은 절댓값(음수) 출력
+                } else {
+                    System.out.println(queue.poll());
+                }
+
+            // 0이 아니면 input을 queue에 넣기
+            } else {
+                queue.add(input);
+            }
+        }
+    }
 }
--- BAEKJOON/DataStructure/Queue/BAEKJOON2164.java
+++ BAEKJOON/DataStructure/Queue/BAEKJOON2164.java
@@ -7,6 +7,35 @@ package BAEKJOON.DataStructure.Queue;
  * 백준 2164번 (https://www.acmicpc.net/problem/2164)
  */
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class BAEKJOON2164 {
-    
+    public static void main(String[] args) throws IOException {
+        // 1. N 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 2. Queue 선언 및 1부터 N까지 채우기
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        // 로직 실행
+        while (queue.size() > 1) {
+            // 1. 맨 끝의 카드 버림
+            queue.remove();
+
+            // 2. 맨 끝의 카드를 맨 처음으로 넣기
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
 }
