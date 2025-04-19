476003402b2d6974cee4d9f6f1739f870d9e1b0c
Boxx-Ham <sangja234@gmail.com>
2025-04-19 14:09:23 +0900

    2025-04-19 : 프로그래머스 해시맵 2 문제 풀이

--- programmers/Problem/DataStructure/HashMap/PROGRAMMERS42579.java
+++ programmers/Problem/DataStructure/HashMap/PROGRAMMERS42579.java
@@ -7,6 +7,67 @@ package PROGRAMMERS.Problem.DataStructure.HashMap;
  * 프로그래머스 42579번 (https://school.programmers.co.kr/learn/courses/30/lessons/42579)
  */
 
+import java.util.Arrays;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PROGRAMMERS42579 {
-    
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+
+        int[] result = solution(genres, plays);
+
+        System.out.println(Arrays.toString(genres) + " / " + Arrays.toString(plays) + " -> " + Arrays.toString(result));
+    }
+
+    static int[] solution(String[] genres, int[] plays) {
+        // 1. 장르별 곡 리스트와 장르별 재생횟수를 담을 HashMap 선언
+        Map<String, ArrayList<int[]>> genresMap = new HashMap<>();
+        Map<String, Integer> playsMap = new HashMap<>();
+
+        // 2. 정답을 담을 ArrayList 선언
+        List<Integer> answer = new ArrayList<>();
+
+        // 3. HashMap에 장르별 곡 리스트와 장르별 재생횟수 담기
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+
+            // 3-1. ArrayList의 경우 초기화를 해주지 않으면 컴파일 오류 발생함
+            if (!genresMap.containsKey(genre)) {
+                genresMap.put(genre, new ArrayList<>());
+            }
+
+            // 3-2. 데이터 담기
+            genresMap.get(genre).add(new int[] {i, play});
+            playsMap.put(genre, playsMap.getOrDefault(playsMap.get(genre), 0) + play);
+        }
+
+        // 4. playsMap을 entry로 만들고 재생횟수를 기준으로 내림차순 정렬하기
+        List<Map.Entry<String, Integer>> categorysEntry = new ArrayList<>(playsMap.entrySet());
+        categorysEntry.sort((c1, c2) -> (c2.getValue().compareTo(c1.getValue())));
+
+        // 5. 정렬한 category를 돌면서 answer에 2개씩 고유 번호 넣기
+        for (Map.Entry<String, Integer> categorys : categorysEntry) {
+            String category = categorys.getKey();
+
+            // 5-1. genresMap에서 장르별 곡 리스트만 가져오기
+            List<int[]> songs = new ArrayList<>(genresMap.get(category));
+
+            // 5-2. 재생횟수를 기준으로 내림차순 정렬
+            songs.sort((s1, s2) -> (s2[1] - s1[1]));    // 1번 인덱스가 재생횟수
+
+            // 5-3. 하나의 장르에서 최대 2개만 가져올 수 있음
+            for (int i = 0; i < Math.min(2, songs.size()); i++) {
+                answer.add(songs.get(i)[0]);
+            }
+        }
+
+        // 6. ArrayList -> Array 후 반환
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
 }
--- programmers/Problem/DataStructure/HashMap/PROGRAMMERS42888.java
+++ programmers/Problem/DataStructure/HashMap/PROGRAMMERS42888.java
@@ -7,6 +7,56 @@ package PROGRAMMERS.Problem.DataStructure.HashMap;
  * 프로그래머스 42888번 (https://school.programmers.co.kr/learn/courses/30/lessons/42888)
  */
 
+import java.util.Arrays;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PROGRAMMERS42888 {
-    
+    public static void main(String[] args) {
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
+        
+        String[] result = solution(record);
+
+        System.out.println(Arrays.toString(record) + " -> " + Arrays.toString(result));
+    }
+
+    static String[] solution(String[] record) {
+        // 1. uid, name 담을 HashMap, 출력 메시지 담을 ArrayList 선언
+        Map<String, String> userMap = new HashMap<>();
+        List<String> msgList = new ArrayList<>();
+        
+        // 2. 명령어 배열을 돌면서 Enter, Change 일 때만 userMap에 uid, name 담기
+        for (String str : record) {
+            // 2-1. 명령어를 세 부분으로 나눠 배열로 저장
+            String[] cmd = str.split(" ");  // 0 : 명령, 1 : uid, 2 : name
+
+            // 2-2. Enter, Change 일 때만 저장 (Enter, Change는 cmd의 길이가 3)
+            if (cmd.length == 3) {
+                userMap.put(cmd[1], cmd[2]);
+            }
+        }
+        
+        // 3. 최종 닉네임을 메시지와 함계 만들어서 msgList에 담기
+        for (String str : record) {
+            // 3-1. 명령어를 세 부분으로 나눠 배열에 저장
+            String[] cmd = str.split(" ");
+
+            // 3-2. Enter, Leave 일 경우의 메시지를 변수로 미리 지정
+            String enterMsg = "님이 들어왔습니다.";
+            String leaveMsg = "님이 나갔습니다.";
+
+            // 3-3. Enter, Leave의 경우에 각각 메시지에 담기
+            if (cmd[0].equals("Enter")) {
+                msgList.add(userMap.get(cmd[1]) + enterMsg);
+            } else if (cmd[0].equals("Leave")) {
+                msgList.add(userMap.get(cmd[1]) + leaveMsg);
+            }
+        }
+
+        // 4. List를 Array로 변환하여 반환
+        return msgList.toArray(new String[0]);  // new String[0]으로 타입 String[]로 선언
+    }
 }
